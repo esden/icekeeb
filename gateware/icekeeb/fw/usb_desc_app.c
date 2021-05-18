e79428b82d7f0f84e7d7846b029c2b8815c8a05d
Sylvain Munaut <tnt@246tNt.com>
2021-05-18 20:28:18 +0200

    fw/usb_desc_app: Add HID descriptor
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- gateware/icekeeb/fw/usb_desc_app.c
+++ gateware/icekeeb/fw/usb_desc_app.c
@@ -24,14 +24,58 @@
 #include <no2usb/usb_proto.h>
 #include <no2usb/usb_cdc_proto.h>
 #include <no2usb/usb_dfu_proto.h>
+#include <no2usb/usb_hid_proto.h>
 #include <no2usb/usb.h>
 
 
 usb_cdc_union_desc_def(1);
 
+const uint8_t app_hid_report_desc[63] = {
+	0x05, 0x01,	/* Usage Page (Generic Desktop) */
+	0x09, 0x06,	/* Usage (Keyboard) */
+	0xa1, 0x01,	/* Collection (Application) */
+	0x05, 0x07,		/* Usage Page (KeyCodes) */
+	0x19, 0xe0,		/* Usage Minimum (224) */
+	0x29, 0xe7,		/* Usage Maximum (231) */
+	0x15, 0x00,		/* Logical Minimum (0) */
+	0x25, 0x01,		/* Logical Maximum (1) */
+	0x75, 0x01,		/* Report Size (1) */
+	0x95, 0x08,		/* Report Count (8) */
+	0x81, 0x02,		/* Input (Data, Variable, Absolute)	Modifier byte */
+	0x95, 0x01,		/* Report Count (1) */
+	0x75, 0x08,		/* Report Size (8) */
+	0x81, 0x01,		/* Input (Constant)			Reserved byte */
+	0x95, 0x05,		/* Report Count (5) */
+	0x75, 0x01,		/* Report Size (1) */
+	0x05, 0x08,		/* Usage Page (Page# for LEDs) */
+	0x19, 0x01,		/* Usage Minimum (1) */
+	0x29, 0x05,		/* Usage Maximum (5) */
+	0x91, 0x02,		/* Output (Data, Variable, Absolute)	LED report */
+	0x95, 0x01,		/* Report Count (1) */
+	0x75, 0x03,		/* Report Size (3) */
+	0x91, 0x01,		/* Output (Constant)			LED report padding */
+	0x95, 0x06,		/* Report Count (6) */
+	0x75, 0x08,		/* Report Size (8) */
+	0x15, 0x00,		/* Logical Minimum (0) */
+	0x25, 0x65,		/* Logical Maximum(101) */
+	0x05, 0x07,		/* Usage Page (Key Codes) */
+	0x19, 0x00,		/* Usage Minimum (0) */
+	0x29, 0x65,		/* Usage Maximum (101) */
+	0x81, 0x00,		/* Input (Data, Array)			Key arrays (6 bytes) */
+	0xc0,		/* End Collection */
+};
+
+
 static const struct {
 	/* Configuration */
 	struct usb_conf_desc conf;
+
+	/* HID */
+	struct {
+		struct usb_intf_desc intf;
+		struct usb_hid_hid_desc hid;
+		struct usb_ep_desc ep_data_in;
+	} __attribute__ ((packed)) hid;
 
 	/* CDC */
 	struct {
@@ -55,23 +99,55 @@ static const struct {
 		.bLength                = sizeof(struct usb_conf_desc),
 		.bDescriptorType        = USB_DT_CONF,
 		.wTotalLength           = sizeof(_app_conf_desc),
-		.bNumInterfaces         = 3,
+		.bNumInterfaces         = 4,
 		.bConfigurationValue    = 1,
 		.iConfiguration         = 4,
 		.bmAttributes           = 0x80,
 		.bMaxPower              = 0x32, /* 100 mA */
 	},
-	.cdc = {
-		.intf_ctl = {
+	.hid = {
+		.intf = {
 			.bLength		= sizeof(struct usb_intf_desc),
 			.bDescriptorType	= USB_DT_INTF,
 			.bInterfaceNumber	= 0,
 			.bAlternateSetting	= 0,
 			.bNumEndpoints		= 1,
+			.bInterfaceClass	= USB_CLS_HID,
+			.bInterfaceSubClass	= USB_HID_SCLS_BOOT,
+			.bInterfaceProtocol	= USB_HID_PROTO_KEYBOARD,
+			.iInterface		= 6,
+		},
+		.hid = {
+			.bLength		= sizeof(struct usb_hid_hid_desc),
+			.bDescriptorType	= USB_HID_DT_HID,
+			.bcdHID			= 0x0101,
+			.bCountryCode		= 0x00,
+			.bNumDescriptors	= 1,
+			.desc[0]		= {
+				.bDescriptorType	= USB_HID_DT_REPORT,
+				.wDescriptorLength	= sizeof(app_hid_report_desc),
+			},
+		},
+		.ep_data_in = {
+			.bLength		= sizeof(struct usb_ep_desc),
+			.bDescriptorType	= USB_DT_EP,
+			.bEndpointAddress	= 0x81,
+			.bmAttributes		= 0x03,
+			.wMaxPacketSize		= 8,
+			.bInterval		= 0x0a,
+		},
+	},
+	.cdc = {
+		.intf_ctl = {
+			.bLength		= sizeof(struct usb_intf_desc),
+			.bDescriptorType	= USB_DT_INTF,
+			.bInterfaceNumber	= 1,
+			.bAlternateSetting	= 0,
+			.bNumEndpoints		= 1,
 			.bInterfaceClass	= USB_CLS_CDC_CONTROL,
 			.bInterfaceSubClass	= USB_CDC_SCLS_ACM,
 			.bInterfaceProtocol	= 0x00,
-			.iInterface		= 5,
+			.iInterface		= 6,
 		},
 		.cdc_hdr = {
 			.bLength		= sizeof(struct usb_cdc_hdr_desc),
@@ -90,7 +166,7 @@ static const struct {
 			.bDescriptorType	= USB_CS_DT_INTF,
 			.bDescriptorsubtype	= USB_CDC_DST_UNION,
 			.bMasterInterface	= 0,
-			.bSlaveInterface	= { 1 },
+			.bSlaveInterface	= { 2 },
 		},
 		.ep_ctl = {
 			.bLength		= sizeof(struct usb_ep_desc),
@@ -103,13 +179,13 @@ static const struct {
 		.intf_data = {
 			.bLength		= sizeof(struct usb_intf_desc),
 			.bDescriptorType	= USB_DT_INTF,
-			.bInterfaceNumber	= 1,
+			.bInterfaceNumber	= 2,
 			.bAlternateSetting	= 0,
 			.bNumEndpoints		= 2,
 			.bInterfaceClass	= USB_CLS_CDC_DATA,
 			.bInterfaceSubClass	= 0x00,
 			.bInterfaceProtocol	= 0x00,
-			.iInterface		= 6,
+			.iInterface		= 7,
 		},
 		.ep_data_out = {
 			.bLength		= sizeof(struct usb_ep_desc),
@@ -132,13 +208,13 @@ static const struct {
 		.intf = {
 			.bLength		= sizeof(struct usb_intf_desc),
 			.bDescriptorType	= USB_DT_INTF,
-			.bInterfaceNumber	= 2,
+			.bInterfaceNumber	= 3,
 			.bAlternateSetting	= 0,
 			.bNumEndpoints		= 0,
 			.bInterfaceClass	= 0xfe,
 			.bInterfaceSubClass	= 0x01,
 			.bInterfaceProtocol	= 0x01,
-			.iInterface		= 7,
+			.iInterface		= 8,
 		},
 		.func = {
 			.bLength		= sizeof(struct usb_dfu_func_desc),
